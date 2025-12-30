0c785ad7c7007c161e1448bd7b96c94e9003719d
Ivo Blok <ivoblokdoorn@gmail.com>
2025-12-30 15:25:10 +0100

    added command line argument to main for better testing

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,13 @@
 #include "FGO.hpp"
 
 int main(int argc, char** argv) {
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " <number_of_atoms>\n";
+        return 1;
+    }
 
     FGOParameters params;
-    params.numberOfAtoms = 4;
+    params.numberOfAtoms = std::stoi(argv[1]);
     
     FuzzyGlobalOptimizer optimizer(params);
 
@@ -16,8 +20,6 @@ int main(int argc, char** argv) {
         std::mt19937 rng(i);
         optimizer.runSingle(rng);
     }
-    
-
     
     return 0;
 }
